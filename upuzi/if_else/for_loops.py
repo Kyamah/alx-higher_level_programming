2112ec061be47cf7e5b5ba99825cf394f854a121
Kyamah <kyamah10@gmail.com>
2024-11-21 01:46:23 -0800

    upuzi

---
+++ upuzi/if_else/for_loops.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+print(list(range(1, 51)))
+
+
+
+c = [20, 30, 40, 50]
+v = 0
+for t in c:
+    v = v + t
+print(v)
+
+
+total = 0
+for e in range(1, 100):
+    if 1 % 3:
+        total += e
+print(total)
+
