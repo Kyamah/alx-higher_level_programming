a32c5b5d8e97012a942f18b437ccce8751acc1fb
Kyamah <kyamah10@gmail.com>
2024-11-21 02:58:19 -0800

    Practice Sessions

---
+++ upuzi/loops/for_loops.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+#print the list inthe list
+
+a = ["banana, mango, apple"]
+for e in a:
+    print(a[0])
+
+#prints the numbers in the list
+
+b = [20, 35, 5]
+for r in b:
+    print(r)
+
+#sums the numbers in the list
+
+c = [12, 20, 30]
+total = 0
+for numbers in c:
+    total = total + numbers
+print(total)
---
+++ upuzi/loops/range_function.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+#using the range function
+
+c = list(range(1, 51))
+print(c)
