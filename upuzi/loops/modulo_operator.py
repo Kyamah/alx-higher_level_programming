fde329e52fd46a48c1224fb5204341da34390d41
Kyamah <kyamah10@gmail.com>
2024-11-25 10:38:39 +0300

    practice

---
+++ upuzi/loops/modulo_operator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+#modulo operator are used to check multiples of numbers
+
+print(4 % 3)
+
+#list numbers for range
+
+print(list(range(1, 21)))
+
+#add numbers for multiples of 3
+
+total = 0
+for y in range(1, 21):
+    if y % 3 == 0:
+        total += y
+print(total)
+
+#add numbers for multiples of 3 and 5
+
+total1 = 0
+for z in range(1, 100):
+    if z % 5 == 0:
+        total1 += z
+print(total1)
--- upuzi/loops/range_function.py
+++ upuzi/loops/range_function.py
@@ -4,3 +4,15 @@
 
 c = list(range(1, 51))
 print(c)
+
+#listing numbers using the range function
+
+for i in range(1, 6):
+    print(i)
+
+#adding numbers using the range function
+
+total = 0
+for x in range(1, 11):
+    total += x
+print (total)
